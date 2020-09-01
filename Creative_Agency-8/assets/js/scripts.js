e8402936311dbbdf3b6e6c821aabd1d419acbd1f
me6edi <exited17@gmail.com>
2020-09-01 19:02:30 +0600

    About section

--- Creative_Agency-8/assets/js/scripts.js
+++ Creative_Agency-8/assets/js/scripts.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function($){
     $(".homepage-slides").owlCarousel({
         items: 1,
         loop: true,
-        nav: true,
-        dots: true,
+        nav: false,
+        dots: false,
         navText: [" <i class='fa fa-long-arrow-left'></i> "," <i class='fa fa-long-arrow-right'></i> "],
-        autoply: false
+        autoply: false,
     });
 });
