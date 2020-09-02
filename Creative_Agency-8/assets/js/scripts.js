b2d672860e136b0f4016a5086958d06910ec9f9a
me6edi <exited17@gmail.com>
2020-09-02 20:47:56 +0600

    Our portfolio

--- Creative_Agency-8/assets/js/scripts.js
+++ Creative_Agency-8/assets/js/scripts.js
@@ -7,4 +7,33 @@ jQuery(document).ready(function($){
         navText: [" <i class='fa fa-long-arrow-left'></i> "," <i class='fa fa-long-arrow-right'></i> "],
         autoply: false,
     });
+
+    $("#web-design-skillbar").circleProgress({
+        value: 0.9,
+        size: 270,
+        thickness: 3,
+        fill: '#22babf',
+    }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('.skill-count-no').html(Math.round(90 * progress) + '<i>%</i>');
+      });
+
+      $("#graphic-design-skillbar").circleProgress({
+        value: 0.8,
+        size: 270,
+        thickness: 3,
+        fill: '#22babf',
+    }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('.skill-count-no').html(Math.round(95 * progress) + '<i>%</i>');
+      });
+
+
+      $("#digital-marketing-skillbar").circleProgress({
+        value: 0.7,
+        size: 270,
+        thickness: 3,
+        fill: '#22babf',
+    }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('.skill-count-no').html(Math.round(84 * progress) + '<i>%</i>');
+      });
+      
 });
